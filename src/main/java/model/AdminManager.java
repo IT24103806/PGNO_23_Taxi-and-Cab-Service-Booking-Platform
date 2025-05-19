5fd67a97712def0fda2527082ae2caa9adc1a28d
IT24104176 <it24104176@my.sliit.lk>
2025-05-20 04:13:45 +0530

    Updated dashboards functionality

--- src/main/java/model/AdminManager.java
+++ src/main/java/model/AdminManager.java
@@ -5,22 +5,36 @@ import java.io.*;
 public class AdminManager {
     private static final String FILE_NAME = "users.txt";
 
-    public static void deleteCustomer(String customerId) throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader(FILE_NAME));
-        BufferedWriter writer = new BufferedWriter(new FileWriter("temp.txt"));
+    public static void deleteCustomer(String customerId) {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(FILE_NAME));
+            BufferedWriter writer = new BufferedWriter(new FileWriter("temp.txt"));
 
-        String line;
-        while ((line = reader.readLine()) != null) {
-            String[] data = line.split(",");
-            if (!data[0].equals(customerId) && data[4].equals("customer")) {
-                writer.write(line + "\n");
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] data = line.split(",");
+                if (!data[0].equals(customerId) && data[4].equals("customer")) {
+                    writer.write(line);
+                    writer.newLine();
+                }
             }
+
+            reader.close();
+            writer.close();
+
+            File oldFile = new File(FILE_NAME);
+            File tempFile = new File("temp.txt");
+
+            if (oldFile.delete()) {
+                if (!tempFile.renameTo(oldFile)) {
+                    System.out.println("Error renaming file.");
+                }
+            } else {
+                System.out.println("Error deleting file.");
+            }
+
+        } catch (IOException e) {
+            System.out.println("An error occurred while deleting the customer: " + e.getMessage());
         }
-
-        reader.close();
-        writer.close();
-
-        new File(FILE_NAME).delete();
-        new File("temp.txt").renameTo(new File(FILE_NAME));
     }
 }
